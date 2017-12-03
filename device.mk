c782f9065a26c7777f697f04eae1d4e74428b9a6
Nikit Sharma <nikitsharma92@gmail.com>
2017-12-03 10:01:49 +0530

    athene: Import OSS camera HAL * CAF Tag: LA.BR.1.3.7_rb1.6

--- device.mk
+++ device.mk
@@ -109,6 +109,8 @@ PRODUCT_COPY_FILES +=  \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.msm8952 \
+    libmm-qcamera \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
     libbson \
